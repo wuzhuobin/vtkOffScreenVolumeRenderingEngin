acc34b94fd784d2a8f8fa57656f938d0ebcff0d8
wuzhuobin <jiejin2022@163.com>
2018-11-20 11:15:55 +0800

    adding benchmark function testing between size and running time.

--- testing/Testvtk_off_screen_volume_rendering_facade.cpp
+++ testing/Testvtk_off_screen_volume_rendering_facade.cpp
@@ -71,8 +71,8 @@ private Q_SLOTS:
   }
   void cleanupTestCase()
   {
-   QFile(this->PNG).remove(); 
-   QFile(this->JPEG).remove();
+    QFile(this->PNG).remove(); 
+    QFile(this->JPEG).remove();
   }
   void init(){}
   void cleanup()
@@ -112,6 +112,56 @@ private Q_SLOTS:
     fout.write((const char*)(data.data()), data.size()* sizeof(char));
     fout.close();
   }
+  void benmarkPNG_data()
+  {
+    QTest::addColumn<QString>("json");
+    QString jsonTemplate = 
+      "{"  
+        "\"preset\":0,"  
+        "\"shift\":0," 
+        "\"opacity\":1," 
+        "\"size\":[%1,%1]" 
+      "}";
+    for(int size = 50; size < 10000; size+=50)
+    {
+      QTest::newRow(QString("Size: [%1,%1]: ").arg(size).toUtf8().constData()) << jsonTemplate.arg(size);
+    }
+  }
+  void benmarkPNG()
+  {
+    QFETCH(QString, json);
+    vtk_off_screen_volume_rendering_facade facade(this->image);
+    QBENCHMARK_ONCE
+    {
+      std::vector<unsigned char> data;
+      facade.render_png(json.toStdString(), data);
+    }
+  } 
+  void benmarkJPEG_data()
+  {
+    QTest::addColumn<QString>("json");
+    QString jsonTemplate = 
+      "{"  
+        "\"preset\":0,"  
+        "\"shift\":0," 
+        "\"opacity\":1," 
+        "\"size\":[%1,%1]" 
+      "}";
+    for(int size = 50; size < 10000; size+=50)
+    {
+      QTest::newRow(QString("Size: [%1,%1]: ").arg(size).toUtf8().constData()) << jsonTemplate.arg(size);
+    }
+  }
+  void benmarkJPEG()
+  {
+    QFETCH(QString, json);
+    vtk_off_screen_volume_rendering_facade facade(this->image);
+    QBENCHMARK_ONCE
+    {
+      std::vector<unsigned char> data;
+      facade.render_jpeg(json.toStdString(), data);
+    }
+  } 
 private:
   vtkSmartPointer<vtkImageData> image;
   const QString PNG = "png.png";
